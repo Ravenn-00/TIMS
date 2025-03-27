d96b31672cc3756ba29ff374e52b78485cd2e57f
GRB <freze000@gmail.com>
2025-03-27 11:21:26 -0500

    TIMS 2 practica

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,10 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
 
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,16 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "str={0}, expected={1}")
+    @CsvSource({ "xxggxx, true","xxgxx, false","xxggyygxx, false","gg, true", "g, false"})
+    public void areAllHappy(String str, boolean expected) {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy(str);
+        Assertions.assertEquals(expected, result);
+    }
 }
